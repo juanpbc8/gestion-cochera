d04a1edf2499733159cb5ac684ae5d9a62e0ff32
joel <joe1ito20tn@gmail.com>
2025-06-20 22:10:21 -0500

    VehiculoController añadido

---
+++ src/main/java/pe/edu/utp/gestion_cochera/controller/VehiculoController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package pe.edu.utp.gestion_cochera.controller;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class VehiculoController {
+
+}
